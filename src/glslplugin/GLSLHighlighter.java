48e1b56d25d73463e5c9bc4cb294d3ad4dda921e
Abigail <abigail@bold.claims>
2015-04-20 08:53:44 +0100

    Remove unnecessary allocation in highlighter
    Previously the syntax highlighter would allocate a new array for every
    single token in the language. Now it keeps a static array for each
    TextAttributesKey, which is more in line with how the custom language
    plugin tutorial and examples do this.

--- src/glslplugin/GLSLHighlighter.java
+++ src/glslplugin/GLSLHighlighter.java
@@ -31,21 +31,37 @@ import org.jetbrains.annotations.NotNull;
 import static glslplugin.lang.elements.GLSLTokenTypes.*;
 
 public class GLSLHighlighter extends SyntaxHighlighterBase {
-    static final TextAttributesKey GLSL_NUMBER = TextAttributesKey.createTextAttributesKey("GLSL.NUMBER", DefaultLanguageHighlighterColors.NUMBER);
-    static final TextAttributesKey GLSL_TYPE_SPECIFIER = TextAttributesKey.createTextAttributesKey("GLSL.TYPE_SPECIFIER", DefaultLanguageHighlighterColors.CLASS_REFERENCE);
-    static final TextAttributesKey GLSL_TYPE_QUALIFIERS = TextAttributesKey.createTextAttributesKey("GLSL.QUALIFIER_TOKENS", DefaultLanguageHighlighterColors.KEYWORD);
-    //static final TextAttributesKey GLSL_PARAMETER_QUALIFIERS = TextAttributesKey.createTextAttributesKey("GLSL.PARAMETER_QUALIFIERS", DefaultLanguageHighlighterColors.KEYWORD);
-    static final TextAttributesKey GLSL_FLOW_KEYWORDS = TextAttributesKey.createTextAttributesKey("GLSL.FLOW_KEYWORDS", DefaultLanguageHighlighterColors.KEYWORD);
-    static final TextAttributesKey GLSL_COMMENT = TextAttributesKey.createTextAttributesKey("GLSL.COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-    static final TextAttributesKey GLSL_IDENTIFIER = TextAttributesKey.createTextAttributesKey("GLSL.IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER);
-    static final TextAttributesKey GLSL_COMPILER_DIRECTIVE = TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE",DefaultLanguageHighlighterColors.DOC_COMMENT_MARKUP);
-    static final TextAttributesKey GLSL_COMPILER_DIRECTIVE_VERSION = TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE_VERSION", GLSL_COMPILER_DIRECTIVE);
-    static final TextAttributesKey GLSL_COMPILER_DIRECTIVE_EXTENSION = TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE_EXTENSION", GLSL_COMPILER_DIRECTIVE);
-    static final TextAttributesKey GLSL_COMPILER_DIRECTIVE_PRAGMA = TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE_PRAGMA", GLSL_COMPILER_DIRECTIVE);
-    static final TextAttributesKey GLSL_COMPILER_DIRECTIVE_OTHER = TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE_OTHER", GLSL_COMPILER_DIRECTIVE);
-    static final TextAttributesKey GLSL_PRECISION_STATEMENT = TextAttributesKey.createTextAttributesKey("GLSL.PRECISION_STATEMENT", GLSL_COMPILER_DIRECTIVE);
-    static final TextAttributesKey GLSL_UNKNOWN = TextAttributesKey.createTextAttributesKey("GLSL.UNKNOWN", HighlighterColors.BAD_CHARACTER);
-    static final TextAttributesKey GLSL_TEXT = TextAttributesKey.createTextAttributesKey("GLSL.TEXT", DefaultLanguageHighlighterColors.DOT);
+
+    static final TextAttributesKey[] GLSL_NUMBER =
+            { TextAttributesKey.createTextAttributesKey("GLSL.NUMBER", DefaultLanguageHighlighterColors.NUMBER) };
+    static final TextAttributesKey[] GLSL_TYPE_SPECIFIER =
+            { TextAttributesKey.createTextAttributesKey("GLSL.TYPE_SPECIFIER", DefaultLanguageHighlighterColors.CLASS_REFERENCE) };
+    static final TextAttributesKey[] GLSL_TYPE_QUALIFIERS =
+            { TextAttributesKey.createTextAttributesKey("GLSL.QUALIFIER_TOKENS", DefaultLanguageHighlighterColors.KEYWORD) };
+    //static final TextAttributesKey[] GLSL_PARAMETER_QUALIFIERS = {
+    //        { TextAttributesKey.createTextAttributesKey("GLSL.PARAMETER_QUALIFIERS", DefaultLanguageHighlighterColors.KEYWORD) };
+    static final TextAttributesKey[] GLSL_FLOW_KEYWORDS =
+            { TextAttributesKey.createTextAttributesKey("GLSL.FLOW_KEYWORDS", DefaultLanguageHighlighterColors.KEYWORD) };
+    static final TextAttributesKey[] GLSL_COMMENT =
+            { TextAttributesKey.createTextAttributesKey("GLSL.COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT) };
+    static final TextAttributesKey[] GLSL_IDENTIFIER =
+            { TextAttributesKey.createTextAttributesKey("GLSL.IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER) };
+    static final TextAttributesKey[] GLSL_COMPILER_DIRECTIVE =
+            { TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE",DefaultLanguageHighlighterColors.DOC_COMMENT_MARKUP) };
+    static final TextAttributesKey[] GLSL_COMPILER_DIRECTIVE_VERSION =
+            { TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE_VERSION", GLSL_COMPILER_DIRECTIVE[0]) };
+    static final TextAttributesKey[] GLSL_COMPILER_DIRECTIVE_EXTENSION =
+            { TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE_EXTENSION", GLSL_COMPILER_DIRECTIVE[0]) };
+    static final TextAttributesKey[] GLSL_COMPILER_DIRECTIVE_PRAGMA =
+            { TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE_PRAGMA", GLSL_COMPILER_DIRECTIVE[0]) };
+    static final TextAttributesKey[] GLSL_COMPILER_DIRECTIVE_OTHER =
+            { TextAttributesKey.createTextAttributesKey("GLSL.COMPILER_DIRECTIVE_OTHER", GLSL_COMPILER_DIRECTIVE[0]) };
+    static final TextAttributesKey[] GLSL_PRECISION_STATEMENT =
+            { TextAttributesKey.createTextAttributesKey("GLSL.PRECISION_STATEMENT", GLSL_COMPILER_DIRECTIVE[0]) };
+    static final TextAttributesKey[] GLSL_UNKNOWN =
+            { TextAttributesKey.createTextAttributesKey("GLSL.UNKNOWN", HighlighterColors.BAD_CHARACTER) };
+    static final TextAttributesKey[] GLSL_TEXT =
+            { TextAttributesKey.createTextAttributesKey("GLSL.TEXT", DefaultLanguageHighlighterColors.DOT) };
 
     @NotNull
     public Lexer getHighlightingLexer() {
@@ -54,31 +70,18 @@ public class GLSLHighlighter extends SyntaxHighlighterBase {
 
     @NotNull
     public TextAttributesKey[] getTokenHighlights(IElementType type) {
-        if (type == INTEGER_CONSTANT || type == FLOAT_CONSTANT || type == BOOL_CONSTANT) {
-            return new TextAttributesKey[]{GLSL_NUMBER};
-        } else if (type == COMMENT_BLOCK || type == COMMENT_LINE) {
-            return new TextAttributesKey[]{GLSL_COMMENT};
-        } else if (type == IDENTIFIER) {
-            return new TextAttributesKey[]{GLSL_IDENTIFIER};
-        } else if (type == COMPILER_DIRECTIVE_VERSION) {
-            return new TextAttributesKey[]{GLSL_COMPILER_DIRECTIVE_VERSION};
-        } else if (type == COMPILER_DIRECTIVE_EXTENSION) {
-            return new TextAttributesKey[]{GLSL_COMPILER_DIRECTIVE_EXTENSION};
-        } else if (type == COMPILER_DIRECTIVE_PRAGMA) {
-            return new TextAttributesKey[]{GLSL_COMPILER_DIRECTIVE_PRAGMA};
-        } else if (type == COMPILER_DIRECTIVE_OTHER) {
-            return new TextAttributesKey[]{GLSL_COMPILER_DIRECTIVE_OTHER};
-        } else if (TYPE_SPECIFIER_NONARRAY_TOKENS.contains(type)) {
-            return new TextAttributesKey[]{GLSL_TYPE_SPECIFIER};
-        } else if (QUALIFIER_TOKENS.contains(type)) {
-            return new TextAttributesKey[]{GLSL_TYPE_QUALIFIERS};
-        } else if (FLOW_KEYWORDS.contains(type)) {
-            return new TextAttributesKey[]{GLSL_FLOW_KEYWORDS};
-        } else if (type == UNKNOWN) {
-            return new TextAttributesKey[]{GLSL_UNKNOWN};
-        }else if (type == PRECISION_STATEMENT){
-            return new TextAttributesKey[]{GLSL_PRECISION_STATEMENT};
-        }
-        return new TextAttributesKey[]{GLSL_TEXT};
+        if (type == INTEGER_CONSTANT || type == FLOAT_CONSTANT || type == BOOL_CONSTANT) return GLSL_NUMBER;
+        if (type == COMMENT_BLOCK || type == COMMENT_LINE) return GLSL_COMMENT;
+        if (type == IDENTIFIER) return GLSL_IDENTIFIER;
+        if (type == COMPILER_DIRECTIVE_VERSION) return GLSL_COMPILER_DIRECTIVE_VERSION;
+        if (type == COMPILER_DIRECTIVE_EXTENSION) return GLSL_COMPILER_DIRECTIVE_EXTENSION;
+        if (type == COMPILER_DIRECTIVE_PRAGMA) return GLSL_COMPILER_DIRECTIVE_PRAGMA;
+        if (type == COMPILER_DIRECTIVE_OTHER) return GLSL_COMPILER_DIRECTIVE_OTHER;
+        if (TYPE_SPECIFIER_NONARRAY_TOKENS.contains(type)) return GLSL_TYPE_SPECIFIER;
+        if (QUALIFIER_TOKENS.contains(type)) return GLSL_TYPE_QUALIFIERS;
+        if (FLOW_KEYWORDS.contains(type)) return GLSL_FLOW_KEYWORDS;
+        if (type == PRECISION_STATEMENT) return GLSL_PRECISION_STATEMENT;
+        if (type == UNKNOWN) return GLSL_UNKNOWN;
+        return GLSL_TEXT;
     }
 }
