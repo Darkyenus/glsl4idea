1b2c12973ec9211661fdb1498c46d46983fa210d
Darkyenus <darkyenus@gmail.com>
2015-05-24 16:43:09 +0200

    Improved the piglit download script

--- DownloadPiglit.sh
+++ DownloadPiglit.sh
@@ -3,7 +3,7 @@
 # Move to this directory
 cd "$(dirname "$0")"
 
-if [ -e piglit-repo ]; then
+if [ -d piglit-repo ]; then
 	echo "Repo exists"
 	cd piglit-repo
 	git pull
@@ -13,10 +13,18 @@ else
 	git clone --depth=1 git://anongit.freedesktop.org/git/piglit piglit-repo
 fi
 
-if [ ! -e piglit ]; then
-	echo Creating piglit directory
-	mkdir piglit
+if [ -d piglit ]; then
+	echo Deleting piglit directory
+	# To clean
+	rm -rf piglit/
 fi
+
+echo Creating piglit directory
+mkdir piglit
+
+mkdir piglit/pass
+mkdir piglit/fail
+mkdir piglit/other
 
 cd piglit-repo
 
@@ -24,16 +32,20 @@ rm -rf ./examples/
 
 for FILE in $(find . -type f -name *.frag) $(find . -type f -name *.vert);
 do
-	if [ -o "-v" ]; then
-		echo "Found $FILE"
-	fi
-	cp "$FILE" "../piglit/$(basename "$FILE")"
+	if [ ! -z "$(grep "expect_result: pass" "$FILE")" ]; then
+		cp "$FILE" "../piglit/pass/$(basename "$FILE")"
+	elif [ ! -z "$(grep "expect_result: fail" "$FILE")" ]; then
+        cp "$FILE" "../piglit/fail/$(basename "$FILE")"
+    else
+        cp "$FILE" "../piglit/other/$(basename "$FILE")"
+    fi
 done
 
 cd ..
-if [ ! -o "-keeprepo" ]; then
+
+if [ "$1" != "keeprepo" ]; then
 	rm -rf piglit-repo
-	echo "Repo deleted (keep with -keeprepo flag)"
+	echo "Repo deleted"
 fi
 
 echo Done
