009f725be191b4c1f284b9c34407a479523d8ebd
Abigail <abigail@bold.claims>
2015-06-17 17:42:34 +0100

    Add type checking for conditional expressions

--- src/glslplugin/lang/elements/expressions/GLSLConditionalExpression.java
+++ src/glslplugin/lang/elements/expressions/GLSLConditionalExpression.java
@@ -20,14 +20,64 @@
 package glslplugin.lang.elements.expressions;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.TokenSet;
+import glslplugin.lang.elements.GLSLTokenTypes;
+import glslplugin.lang.elements.types.GLSLType;
+import glslplugin.lang.elements.types.GLSLTypes;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Represents an expression of the form `condition ? trueBranch : falseBranch`.
  * @author abigail
  */
 public class GLSLConditionalExpression extends GLSLExpression {
+
+    private static final TokenSet QUESTION = TokenSet.create(GLSLTokenTypes.QUESTION);
+    private static final TokenSet COLON = TokenSet.create(GLSLTokenTypes.COLON);
+
     public GLSLConditionalExpression(@NotNull ASTNode astNode) {
         super(astNode);
+    }
+
+    // For robustness, we find each of the parts of the conditional by starting at the question mark
+    // or the colon and walking left or right.
+    @Nullable
+    public GLSLExpression getCondition() {
+        PsiElement condition = findChildByType(QUESTION);
+        while (condition != null && !(condition instanceof GLSLExpression)) {
+            condition = condition.getPrevSibling();
+        }
+        return (GLSLExpression) condition;
+    }
+
+    @Nullable
+    public GLSLExpression getTrueBranch() {
+        PsiElement condition = findChildByType(QUESTION);
+        while (condition != null && !(condition instanceof GLSLExpression)) {
+            condition = condition.getNextSibling();
+        }
+        return (GLSLExpression) condition;
+    }
+
+    @Nullable
+    public GLSLExpression getFalseBranch() {
+        PsiElement condition = findChildByType(COLON);
+        while (condition != null && !(condition instanceof GLSLExpression)) {
+            condition = condition.getNextSibling();
+        }
+        return (GLSLExpression) condition;
+    }
+
+    @NotNull
+    @Override
+    public GLSLType getType() {
+        final GLSLExpression trueBranch = getTrueBranch();
+        final GLSLType trueType = trueBranch != null ? trueBranch.getType() : GLSLTypes.UNKNOWN_TYPE;
+        final GLSLExpression falseBranch = getFalseBranch();
+        final GLSLType falseType = falseBranch != null ? falseBranch.getType() : GLSLTypes.UNKNOWN_TYPE;
+
+        return GLSLTypes.unifyTypes(trueType, falseType);
     }
 }
