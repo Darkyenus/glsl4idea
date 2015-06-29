084642917cb695d6b3e6fa59f20c9b4e01a40218
Darkyenus <darkyenus@gmail.com>
2015-06-29 23:05:21 +0200

    Removed unused element types

--- src/glslplugin/lang/elements/GLSLElementTypes.java
+++ src/glslplugin/lang/elements/GLSLElementTypes.java
@@ -24,21 +24,13 @@ import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.IFileElementType;
 import glslplugin.lang.GLSLLanguage;
 import glslplugin.lang.elements.expressions.GLSLLiteral;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
 
 public class GLSLElementTypes {
     public static final IFileElementType FILE = new IFileElementType(Language.findInstance(GLSLLanguage.class));
-    public static final IElementType BLOCK = new GLSLElementType("BLOCK");
 
     public static final IElementType PREPROCESSOR_DIRECTIVE = new GLSLElementType("PREPROCESSOR_DIRECTIVE");
 
-    public static final IElementType EMPTY_STATEMENT = new GLSLElementType("EMPTY_STATEMENT");
-    public static final IElementType JUMP_STATEMENT = new GLSLElementType("JUMP_STATEMENT");
-
     public static final IElementType VARIABLE_NAME_EXPRESSION = new GLSLElementType("VARIABLE_NAME_EXPRESSION");
-    public static final IElementType PRIMARY_EXPRESSION = new GLSLElementType("PRIMARY_EXPRESSION");
-    public static final IElementType POSTFIX_EXPRESSION = new GLSLElementType("POSTFIX_EXPRESSION");
     public static final IElementType EXPRESSION = new GLSLElementType("EXPRESSION");
     public static final IElementType CONSTANT_EXPRESSION = new GLSLElementType("CONSTANT_EXPRESSION");
     public static final IElementType GROUPED_EXPRESSION = new GLSLElementType("GROUPED_EXPRESSION");
@@ -54,11 +46,7 @@ public class GLSLElementTypes {
     public static final IElementType FUNCTION_NAME = new GLSLElementType("FUNCTION_NAME");
 
     public static final IElementType FUNCTION_DECLARATION = new GLSLElementType("FUNCTION_DECLARATION");
-    public static final IElementType FUNCTION_PARAMETER_DECLARATOR = new GLSLElementType("FUNCTION_PARAMETER_DECLARATOR");
-    public static final IElementType FUNCTION_PARAMETER_DECLARATOR_LIST = new GLSLElementType("FUNCTION_PARAMETER_DECLARATOR_LIST");
-    public static final IElementType PARAMETER_QUALIFIER = new GLSLElementType("PARAMETER_QUALIFIER");
 
-    public static final IElementType STRUCT_SPECIFIER = new GLSLElementType("STRUCT_SPECIFIER");
     public static final IElementType STRUCT_DECLARATION_LIST = new GLSLElementType("STRUCT_DECLARATION_LIST");
     public static final IElementType STRUCT_DECLARATION = new GLSLElementType("STRUCT_DECLARATION");
     public static final IElementType STRUCT_DECLARATOR = new GLSLElementType("STRUCT_DECLARATOR");
@@ -67,9 +55,7 @@ public class GLSLElementTypes {
     public static final IElementType TYPE_SPECIFIER_PRIMITIVE = new GLSLElementType("TYPE_SPECIFIER_PRIMITIVE");
     public static final IElementType TYPE_SPECIFIER_STRUCT = new GLSLElementType("TYPE_SPECIFIER_STRUCT");
     public static final IElementType TYPE_SPECIFIER_STRUCT_REFERENCE = new GLSLElementType("TYPE_SPECIFIER_STRUCT_REFERENCE");
-//    public static final IElementType TYPE_SPECIFIER_NAMED = new GLSLElementType("TYPE_SPECIFIER_NAMED");
 
-    public static final IElementType INIT_DECLARATOR_LIST = new GLSLElementType("INIT_DECLARATOR_LIST");
     public static final IElementType VARIABLE_DECLARATION = new GLSLElementType("VARIABLE_DECLARATION");
     public static final IElementType INTERFACE_BLOCK = new GLSLElementType("INTERFACE_BLOCK");
     public static final IElementType FUNCTION_DEFINITION = new GLSLElementType("FUNCTION_DEFINITION");
@@ -82,7 +68,6 @@ public class GLSLElementTypes {
     public static final IElementType PARAMETER_DECLARATION_LIST = new GLSLElementType("PARAMETER_DECLARATION_LIST");
     public static final IElementType PARAMETER_DECLARATION = new GLSLElementType("PARAMETER_DECLARATION");
     public static final IElementType PARAMETER_DECLARATOR = new GLSLElementType("PARAMETER_DECLARATOR");
-    public static final IElementType FULLY_SPECIFIED_PARAMETER_TYPE = new GLSLElementType("FULLY_SPECIFIED_PARAMETER_TYPE");
 
 
     public static final IElementType DECLARATOR_LIST = new GLSLElementType("DECLARATOR_LIST");
@@ -92,9 +77,6 @@ public class GLSLElementTypes {
     public static final IElementType INITIALIZER_LIST = new GLSLElementType("INITIALIZER_LIST");
 
     public static final IElementType COMPOUND_STATEMENT = new GLSLElementType("COMPOUND_STATEMENT");
-
-    public static final IElementType ASSIGNMENT_OPERATOR = new GLSLElementType("ASSIGNMENT_OPERATOR");
-    public static final IElementType UNARY_OPERATOR = new GLSLElementType("UNARY_OPERATOR");
 
     //    public static final IElementType UNARY_EXPRESSION = new GLSLElementType("UNARY_EXPRESSION");
     public static final IElementType ASSIGNMENT_EXPRESSION = new GLSLElementType("ASSIGNMENT_EXPRESSION");
@@ -113,9 +95,6 @@ public class GLSLElementTypes {
 
     public static final IElementType FUNCTION_CALL_EXPRESSION = new GLSLElementType("FUNCTION_CALL_EXPRESSION");
     public static final IElementType PARAMETER_LIST = new GLSLElementType("PARAMETER_LIST");
-    public static final IElementType STATEMENT_LIST = new GLSLElementType("STATEMENT_LIST");
-    public static final IElementType STATEMENT = new GLSLElementType("STATEMENT");
-    public static final IElementType SIMPLE_STATEMENT = new GLSLElementType("SIMPLE_STATEMENT");
     public static final IElementType EXPRESSION_STATEMENT = new GLSLElementType("EXPRESSION_STATEMENT");
     public static final IElementType DECLARATION_STATEMENT = new GLSLElementType("DECLARATION_STATEMENT");
 
@@ -131,8 +110,6 @@ public class GLSLElementTypes {
 
     public static final IElementType PRECISION_STATEMENT = new GLSLElementType("PRECISION_STATEMENT");
 
-    public static final IElementType FOR_INIT_STATEMENT = new GLSLElementType("FOR_INIT_STATEMENT");
-    public static final IElementType FOR_REST_STATEMENT = new GLSLElementType("FOR_REST_STATEMENT");
     public static final IElementType CONDITION = new GLSLElementType("CONDITION");
 
     //Preprocessor dropins
