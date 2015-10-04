dea0688854826589ea5ae13feca27080218f8f9d
Darkyenus <darkyenus@gmail.com>
2015-10-04 10:01:34 +0200

    Make GLSLFlexAdapter thread safe
    This prevents errors when flex fails to parse valid input due to multiple
    IntelliJ threads acessing it at once.

--- src/glslplugin/lang/scanner/GLSLFlexAdapter.java
+++ src/glslplugin/lang/scanner/GLSLFlexAdapter.java
@@ -19,10 +19,101 @@
 
 package glslplugin.lang.scanner;
 
-import com.intellij.lexer.FlexAdapter;
+import com.intellij.lexer.LexerBase;
+import com.intellij.lexer.LexerPosition;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
 
-public class GLSLFlexAdapter extends FlexAdapter {
+import java.io.IOException;
+
+/**
+ * based on com.intellij.lexer.FlexAdapter
+ */
+public final class GLSLFlexAdapter extends LexerBase {
+
     public GLSLFlexAdapter() {
-        super(new GLSLFlexLexer(null));
+        myFlex = new GLSLFlexLexer(null);
+    }
+
+    private GLSLFlexLexer myFlex = null;
+    private IElementType myTokenType = null;
+    private CharSequence myText;
+
+    private int myEnd;
+    private int myState;
+
+    @Override
+    public synchronized void start(@NotNull final CharSequence buffer, int startOffset, int endOffset, final int initialState) {
+        myText = buffer;
+        myEnd = endOffset;
+        myFlex.reset(myText, startOffset, endOffset, initialState);
+        myTokenType = null;
+    }
+
+    @Override
+    public synchronized int getState() {
+        if (myTokenType == null) locateToken();
+        return myState;
+    }
+
+    @Override
+    public synchronized IElementType getTokenType() {
+        if (myTokenType == null) locateToken();
+        return myTokenType;
+    }
+
+    @Override
+    public synchronized int getTokenStart() {
+        if (myTokenType == null) locateToken();
+        return myFlex.getTokenStart();
+    }
+
+    @Override
+    public synchronized int getTokenEnd() {
+        if (myTokenType == null) locateToken();
+        return myFlex.getTokenEnd();
+    }
+
+    @Override
+    public synchronized void advance() {
+        if (myTokenType == null) locateToken();
+        myTokenType = null;
+    }
+
+    @NotNull
+    @Override
+    public synchronized CharSequence getBufferSequence() {
+        return myText;
+    }
+
+    @Override
+    public synchronized int getBufferEnd() {
+        return myEnd;
+    }
+
+    private void locateToken() {
+        if (myTokenType != null) return;
+        try {
+            myState = myFlex.yystate();
+            myTokenType = myFlex.advance();
+        }
+        catch (IOException e) { /*Can't happen*/ }
+        catch (Error e) {
+            // add lexer class name to the error
+            final Error error = new Error(myFlex.getClass().getName() + ": " + e.getMessage());
+            error.setStackTrace(e.getStackTrace());
+            throw error;
+        }
+    }
+
+    @NotNull
+    @Override
+    public synchronized LexerPosition getCurrentPosition() {
+        return super.getCurrentPosition();
+    }
+
+    @Override
+    public synchronized void restore(@NotNull LexerPosition position) {
+        super.restore(position);
     }
 }
