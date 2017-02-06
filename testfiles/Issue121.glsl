f7e85fbea21e170dadd7cb7f0af6870a7d6a73af
Darkyenus <darkyenus@gmail.com>
2017-02-06 11:52:49 +0100

    Add new testfile
    Took 3 minutes

---
+++ testfiles/Issue121.glsl
@@ -0,0 +1,7 @@
+#version 120
+
+const int SIZE = SIZE;
+
+const int sampleSize = SIZE * * ; //This should highlight as error and not freeze the IDE
+
+int thing = foo * * * * + + + * * * - - ^ ~ bar;
